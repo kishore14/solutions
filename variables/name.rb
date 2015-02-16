e9b40827c5d7f5c946d5543bc9d93c180e5aba35
kishore14 <kishore.rambatla@gmail.com>
2015-02-16 22:30:26 +0000

    Solutions for variables exercise

---
+++ variables/name.rb
@@ -0,0 +1,11 @@
+puts "whats your first name?"
+first_name = gets.chomp
+
+puts "whats yoru last name"
+last_name =gets.chomp
+puts "Hi "+first_name+' '+last_name
+
+
+#10.times do
+ # puts name
+#end
