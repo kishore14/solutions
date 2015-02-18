db4a5f6cd1d5656ecf826d2c8307c9eb22981d74
kishore14 <kishore.rambatla@gmail.com>
2015-02-18 21:43:40 +0000

    Arrays

---
+++ arrays/create_new.rb
@@ -0,0 +1,8 @@
+#create_new array
+
+a=[1,2,3]
+
+b=a.map{|e| e+2}
+
+p a
+p b
---
+++ arrays/exists.rb
@@ -0,0 +1,12 @@
+#exists.rb
+#check to see if number exists or not.
+
+arr=[1,3,5,7,9,11]
+number = 2
+
+arr.each do |e|
+  if e == number
+    puts"#{number} exists in arr"
+  
+  end
+end
